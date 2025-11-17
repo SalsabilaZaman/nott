42a1652aff751ca6415dc90682346e4ad4eea899
SalsabilaZaman <salsabilazamn@gmail.com>
2025-11-17 15:54:17 +0600

    Implement user registration, login, and profile retrieval functionality; add user model with password hashing and validation

---
+++ backend/controllers/userController.js
@@ -0,0 +1,76 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+//@desc Register a new user
+// @route POST /api/users/register
+// @access Public
+const registerUser = asyncHandler(async(req, res) => {
+  const { username, email, password } = req.body;
+
+  // Basic validation
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("Please provide all required fields");
+  }
+
+  // Check if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists!");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log(hashedPassword);
+  // Create new user
+//   const newUser = await User.create({
+//     username: username.trim(),
+//     email: email.trim(),
+//     password: hashedPassword,
+//   });
+
+  res.status(201).json({ message: "User registered successfully", user: newUser });
+});
+
+// @desc Login a user
+// @route POST /api/users/login
+// @access Public
+const loginUser = asyncHandler(async(req, res) => {
+  const { email, password } = req.body;
+
+  // Basic validation
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please provide all required fields");
+  }
+
+  // Check for user
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.status(200).json({ message: "User logged in successfully", user });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async(req, res) => {
+  const user = await User.findById(req.user.id).select('-password');
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getUserProfile,
+};
+
