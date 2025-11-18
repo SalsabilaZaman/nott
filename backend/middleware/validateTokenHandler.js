f92babd52810fe65eada4486fc65cbe880e6a6a9
SalsabilaZaman <salsabilazamn@gmail.com>
2025-11-18 15:19:29 +0600

    Refactor user authentication logic and add token validation middleware

---
+++ backend/middleware/validateTokenHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+
+
+const validateToken = asyncHandler(async (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization ;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.status(401);
+    throw new Error("No token provided");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // Attach decoded user info to request object
+    next();
+  } catch (error) {
+    res.status(401);
+    throw new Error("Invalid token");
+  }
+});
+
+module.exports = validateToken; 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -20,14 +20,14 @@ const userSchema = new mongoose.Schema({
 });
 
 // Pre-save hook to hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) {
+//     return next();
+//   }
+//   const salt = await bcrypt.genSalt(10);
+//   this.password = await bcrypt.hash(this.password, salt);
+//   next();
+// });
 
 // Method to compare entered password with hashed password
 userSchema.methods.matchPassword = async function (enteredPassword) {
